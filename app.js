63d9fffc87e4e1ce063d9058b93972391c612567
Manmadh-45 <84560628+Manmadh-45@users.noreply.github.com>
2025-03-22 12:55:25 +0530

    basic routing

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/',(req,res)=>{
+    res.send('nothing');
+});
+
+app.get('/name',(req,res)=>{
+    res.send('name')
+});
+
+app.get('/abc',(req,res)=>{
+    res.send('abc');
+});
+
 app.use((req,res)=>{
     res.send('Hello world');
 });
